35d102d797f664e3e57c7a750395481534f7a3ac
AirrQu0tes <hastings.james.m@gmail.com>
2019-02-04 01:36:42 +1100

    Email can be sent on GET request to url

--- controllers/public.js
+++ controllers/public.js
@@ -1,13 +1,14 @@
 // EXPRESS ROUTER: Public pages to show all users and guests
 const router = require('express').Router();
-
 // MONGOOSE: Connect to MongoDB via Mongoose and reference Organisation collection
 const mongoose = require('mongoose');
+
 const User = require('../models/User')
+const nodemailer = require('nodemailer'); 
 
 
 // PUBLIC ROUTES: Root
-router.get('/', (req,res) => {
+router.get('/', (req, res) => {
   return res.send("public route working")
 })
 
@@ -18,6 +19,34 @@ router.get('/admin', (req, res) => {
 router.get('/users', (req, res) => {
   User.find()
     .then(docs => res.send(docs));
+})
+
+router.get('/email', (req, res) => {
+
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: process.env.GMAIL_ACCOUNT,
+        pass: process.env.GMAIL_PASS
+    }
+  })
+
+  const emailBody = {
+    from: 'askizzyplustest1@gmail.com',
+    to: 'askizzyplustest1@gmail.com',
+    subject: 'PublicRoute Email Test',
+    html: '<p>This email is sent via GET request from /email route</p>'
+    }
+
+transporter.sendMail(emailBody, function(error, info) {
+  if(error){
+    console.log(error)
+    res.send('Email not sent :(')
+ } else {
+    console.log(info.response)
+    res.send('Email Sent :-)')
+}
+})
 })
 
 module.exports = router;
